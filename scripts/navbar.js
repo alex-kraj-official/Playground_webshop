bfd6bbf77ae800d5d30724297e510a6fb663eb76
Alex Kráj <alex.kraj.official@gmail.com>
2025-02-25 14:35:58 +0100

    Playground webshop v0.46
    Created a navbar.js to simplify and unify the links on the navbar buttons.

---
+++ scripts/navbar.js
@@ -0,0 +1,23 @@
+let dot; //stores the correct No. dots into the path
+let navLinks = {}; //stores the correct paths on the navbar buttons
+
+setNavLinks(); //creates the correct navbar links onto the navbar buttons
+
+//Creates the correct navbar links onto the navbar buttons
+function setNavLinks(){
+    if(currentHtmlPageFilename.includes("products") ? dot = ".." : dot = "."); //check if the current page is a products page and stores the correct No. dots into the path
+    //create the correct navLinks
+    navLinks = {
+        home: `${dot}/home.html`,
+        about: `${dot}/about.html`,
+        products: `${dot}/products/products_1.html`,
+        contact: `${dot}/contact.html`,
+        cart: `${dot}/cart.html`
+    };
+    //set the correct navLinks
+    document.getElementById("home-link").href = navLinks.home;
+    document.getElementById("about-link").href = navLinks.about;
+    document.getElementById("products-link").href = navLinks.products;
+    document.getElementById("contact-link").href = navLinks.contact;
+    document.getElementById("cart-link").href = navLinks.cart;
+}
